0ff56e996fd575a9558b5c9724f6efe0da533964
maciek30-byte <maciek29.opozda@gmail.com>
2021-05-22 15:30:13 +0200

    Add project files.

---
+++ Components/Contact.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook.Components
+{
+    class Contact
+    {
+        private int Id;
+        public string Name;
+        public string Surname;
+        private string PhoneNumber;
+        private string ContactInformation;
+
+        public string contactInformation
+        {
+            get { return ContactInformation; }
+            set
+            {
+                if (value.Length > 9)
+                {
+                    Console.WriteLine("To loong phone number");
+                }
+                ContactInformation = value;
+            }
+        }
+        public Contact(string name, string surname, string phoneNumber)
+        {
+            Name = name;
+            Surname = surname;
+            SetPhoneNumber(phoneNumber);
+        }
+        public string GetPhoneNumber() => PhoneNumber;
+        public int GetId() => Id;
+        public void SetPhoneNumber(string newNumber)
+        {
+            PhoneNumber = newNumber;
+        }
+
+
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using PhoneBook.Components;
+
+namespace PhoneBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contact maciek = new Contact("Maciek", "Opozda", "531-256-638646546465465456465456");
+            maciek.contactInformation = "czesto dzwoni ";
+
+
+
+
+            Console.Write($" name : {maciek.Name} surname :{maciek.Surname} and phone number is {maciek.GetPhoneNumber()} and your note about this contact is {maciek.contactInformation} ");
+
+
+
+        }
+    }
+}
